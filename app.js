be24e162fa5816403747356864c3cef277569f6c
Fakhre23 <fakhretamimie@gmail.com>
2025-11-09 15:47:54 +0300

    conditions tamplates and methods

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const app = Vue.createApp({ //that called root component 
-    //data, functions 
-     data() {
-        return {
-            title: 'the final empire',
-            author: 'Fakhre',
-            age: '21',
-        }
-     }
-})
+const app = Vue.createApp({
+  //that called root component
+  //data, functions
+  data() {
+    return {
+      showBooks: true,
+      title: "the final empire",
+      author: "Fakhre",
+      age: "21",
+      Books: "this is to many books here",
+    };
+  },
+  methods: {
+    changeTitle(title) {
+      //why we type .this ? because its references the title inside the component itself
+      this.title = title;
+    },
 
-app.mount('#app')
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+  },
+});
+
+app.mount("#app");
